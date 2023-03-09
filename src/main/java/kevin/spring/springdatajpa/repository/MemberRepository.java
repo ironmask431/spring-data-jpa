7bd50e2c6719c26de048b5e79b5b72b97c3b3966
ironmask431 <ironmask431@gmail.com>
2023-03-09 23:14:30 +0900

    공통인터페이스 설정

--- src/main/java/kevin/spring/springdatajpa/repository/MemberRepository.java
+++ src/main/java/kevin/spring/springdatajpa/repository/MemberRepository.java
@@ -5,6 +5,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 /**
  * spring data jpa 는 인터페이스로 repository 선언 후 extends JpaRepository 해주면 완료
+ * 개발자는 인터페이스만 선언해놓으면 spring data jpa가 자동으로 구현체를 만든다.
+ * @Repository 어노테이션을 생략가능하다.
  */
 public interface MemberRepository extends JpaRepository<Member, Long> {
 
