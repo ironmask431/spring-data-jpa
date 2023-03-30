e3b4ecb1b8a49cdb1a3eeaa178592c86f11ba4eb
ironmask431 <ironmask431@gmail.com>
2023-03-31 08:50:32 +0900

    Lock

--- src/test/java/kevin/spring/springdatajpa/repository/MemberRepositoryTest.java
+++ src/test/java/kevin/spring/springdatajpa/repository/MemberRepositoryTest.java
@@ -4,7 +4,6 @@ import kevin.spring.springdatajpa.dto.MemberDto;
 import kevin.spring.springdatajpa.entity.Member;
 import kevin.spring.springdatajpa.entity.Team;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
